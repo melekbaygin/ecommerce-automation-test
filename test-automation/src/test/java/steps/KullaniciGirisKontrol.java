4024b3ac08794ae53d66fbcaac393394ec278d02
Melek Kus <melekbaygin@outlook.com>
2025-07-02 01:37:05 +0300

    add user input control

---
+++ test-automation/src/test/java/steps/KullaniciGirisKontrol.java
@@ -0,0 +1,45 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utilities.DriverFactory;
+
+import pages.LoginPage;
+
+import static org.junit.Assert.assertTrue;
+
+public class KullaniciGirisKontrol {
+    private WebDriver driver;
+    private LoginPage loginPace;
+
+    @Given("Kullanıcı {string} adresindeki e-ticaret sitesini ziyaret eder")
+    public void kullanıcı_adresindeki_e_ticaret_sitesini_ziyaret_eder(String url) {
+        driver = DriverFactory.getDriver(System.getProperty("browser", "chrome"));
+        driver.manage().window().maximize();
+        driver.get(url);
+        loginPace = new LoginPage(driver);
+    }
+    @When("Kullanıcı Giriş Yap butonuna tıklar")
+    public void kullanıcı_giriş_yap_butonuna_tıklar() {
+        loginPace.clickLoginLink();
+    }
+    @When("Kullanıcı e-posta adresine {string} yazar")
+    public void kullanıcı_e_posta_adresine_yazar(String email) {
+        loginPace.enterEmail(email);
+    }
+    @When("Kullanıcı şifre olarak {string} şifresi ile giriş yapar")
+    public void kullanıcı_şifre_olarak_şifresi_ile_giriş_yapar(String password) {
+        loginPace.enterPassword(password);
+        loginPace.submitLogin();
+
+    }
+    @Then("Ana sayfa görüntülenir")
+    public void ana_sayfa_görüntülenir() {
+        assertTrue(loginPace.isHomePageVisible());
+    }
+}
