80c9502e30ba2ca277c6c699ef772560347dcc59
Melek Kus <melekbaygin@outlook.com>
2025-07-02 01:36:09 +0300

    add test runner class

---
+++ test-automation/src/test/java/runners/TestRunner.java
@@ -0,0 +1,26 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = {
+            "src/test/java/features/1-KullaniciGirisKontrol.feature",
+            "src/test/java/features/2-HataliGiris.feature",
+
+    },
+    glue = "steps",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber-html-report.html",
+        "json:target/cucumber-reports/cucumber.json",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    },
+    snippets = CucumberOptions.SnippetType.CAMELCASE,
+    tags = "@ui or @api"
+)
+public class TestRunner {
+
+}
