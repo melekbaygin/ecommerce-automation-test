e6fc1b1207a964bd05e1066aa6ac6b3815b837db
Melek Kus <melekbaygin@outlook.com>
2025-07-05 00:31:11 +0300

    add ability for users to enter the driver as a parameter

--- test-automation/src/test/java/utilities/DriverFactory.java
+++ test-automation/src/test/java/utilities/DriverFactory.java
@@ -4,26 +4,40 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
-    public static WebDriver getDriver(String browserName) {
+    public static WebDriver getDriver() {
+        // Tarayıcı adı dışarıdan alınır, default olarak "edge"
+        String browserName = System.getProperty("browser", "chrome").toLowerCase();
         WebDriver driver;
-        switch (browserName.toLowerCase()) {
+
+        switch (browserName) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
                 driver = new ChromeDriver(chromeOptions);
                 break;
+
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
                 driver = new FirefoxDriver(firefoxOptions);
                 break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                driver = new EdgeDriver(edgeOptions);
+                break;
+
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browserName);
         }
+
         return driver;
     }
 }
